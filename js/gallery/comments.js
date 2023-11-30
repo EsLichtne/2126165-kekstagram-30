615f0d8bab0520f6b2f9d54a60e053ab19185f55
Ксения <easyghoul@gmail.com>
2023-11-30 16:30:25 +0700

    Добавляет функционал отправки комментария пользователем

--- js/gallery/comments.js
+++ js/gallery/comments.js
@@ -3,6 +3,8 @@ const template = document.querySelector('.social__comment');
 const shownCounter = document.querySelector('.social__comment-shown-count');
 const totalCounter = document.querySelector('.social__comment-total-count');
 const loaderButton = document.querySelector('.social__comments-loader');
+const field = document.querySelector('.social__footer-text');
+const sendButton = document.querySelector('.social__footer-btn');
 
 const createComments = (commentsData) => commentsData.map((properties) => {
   const {avatar, name, message} = properties;
@@ -19,9 +21,10 @@ const renderComments = (commentsData, step = 5) => {
   const currentCommentsData = [...commentsData];
 
   const onLoaderButtonClick = () => {
+    const userComments = container.querySelectorAll('.user-comment');
     container.append(...createComments(currentCommentsData.splice(0, step)));
-    shownCounter.textContent = commentsData.length - currentCommentsData.length;
-    totalCounter.textContent = commentsData.length;
+    shownCounter.textContent = commentsData.length - currentCommentsData.length + userComments.length;
+    totalCounter.textContent = commentsData.length + userComments.length;
     loaderButton.classList.toggle('hidden', !currentCommentsData.length);
   };
 
@@ -34,5 +37,28 @@ const renderComments = (commentsData, step = 5) => {
     loaderButton.removeEventListener('click', onLoaderButtonClick);
   }, {once: true});
 };
+
+const onSendButtonClick = () => {
+  if (field.value) {
+    const text = field.value;
+    const comment = template.cloneNode(true);
+    comment.querySelector('.social__picture').src = '../../img/avatar-6.svg';
+    comment.querySelector('.social__text').textContent = text;
+    comment.classList.add('user-comment');
+    container.insertAdjacentElement('afterbegin', comment);
+    field.value = '';
+    shownCounter.textContent++;
+    totalCounter.textContent++;
+  }
+};
+
+const onDocumentKeydown = (event) => {
+  if (event.key.startsWith('Ent')) {
+    sendButton.click();
+  }
+};
+
+sendButton.addEventListener('click', onSendButtonClick);
+document.addEventListener('keydown', onDocumentKeydown);
 
 export {renderComments};
